693721b2f8211ad8c52853e721c6aa7c97fb80e5
Saikat Maiti <saikat.geo@gmail.com>
2018-07-25 19:42:25 +0530

    added main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, session, g, redirect, \
+    url_for, abort, render_template, flash
+import os
+import requests
+from datetime import date, timedelta
+import json
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.from_envvar('APP_CONFIG_FILE', silent=True)
+
+##MAPBOX_ACCESS_KEY = app.config['MAPBOX_ACCESS_KEY']
+
+@app.route('/')
+def index():
+        rainData = getJson()
+        return render_template('index.html', rainData= rainData)
+
+def getJson():
+        accumulated_rainfall = "precip_3hr"
+	yesterday = date.today() - timedelta(1)
+	startTime = yesterday.strftime('%Y-%m-%d')
+	endTime = str(date.today())
+	url = 'https://pmmpublisher.pps.eosdis.nasa.gov/opensearch?q='+accumulated_rainfall+'&lat=60&lon=180&limit=2&startTime='+startTime+'&endTime='+endTime+''
+	data = requests.get(url).json()
+	format_data = data['items'][0]['action'][5]
+	#Extract geojson url from request
+	rainData = format_data['using'][0]['url']
+	return rainData
+if __name__ == '__main__':
+    app.run()
